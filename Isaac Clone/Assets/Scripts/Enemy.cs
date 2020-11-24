2c9f18f344c2240b66e145fed635816bff853747
GianniLC <70944324+GianniLC@users.noreply.github.com>
2020-11-24 19:32:34 +0100

    Player And Enemies Project
    Unity Project including player movement and enemy scripts

---
+++ Isaac Clone/Assets/Scripts/Enemy.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour{
+
+    [Header("Enemy movement value")]
+    public float enemyMovementSpeed;
+    public float stoppingDistance;
+    public float retreatDistance;
+
+    [Header("Enemy Healthvalues")]
+    public float health;
+
+    [Header("Enemy shooting values")]
+    public GameObject projectTile;
+
+    private float timeBetweenShots;
+    public float startTimeBetweenShot;
+
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        timeBetweenShots = startTimeBetweenShot;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check to see if our enemy distance relative to the player is greater than our stopping distance
+        if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
+        {
+            // move the enemy position towards the player position
+            transform.position = Vector2.MoveTowards(transform.position, player.position, enemyMovementSpeed * Time.deltaTime);
+        }
+        else if (Vector2.Distance(transform.position, player.position) < stoppingDistance && (Vector2.Distance(transform.position, player.position) > retreatDistance))
+        {
+            transform.position = this.transform.position;
+        }
+        else if (Vector2.Distance(transform.position, player.position) < retreatDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -enemyMovementSpeed * Time.deltaTime);
+        }
+
+        if(timeBetweenShots <= 0)
+        {
+            // Instantiate means to spawn a item with the values at what position and what rotation
+            Instantiate(projectTile, transform.position, quaternion.identity);
+            timeBetweenShots = startTimeBetweenShot; // remove this and he shoots every single frame
+        }
+        else
+        {
+            timeBetweenShots -= Time.deltaTime;
+        }
+
+        EnemyDeath();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("PlayerProjectile"))
+        {
+            health = health - 1;
+        }
+    }
+
+    public void EnemyDeath()
+    {
+        if(health == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Isaac Clone/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour{
+
+    [Header("Player Values")]
+    public float health;
+    public float attackDamage;
+
+    public float movementSpeed;
+
+    [Header("ProjectileDamageTaken")]
+    public float projectileDamage;
+
+    [Header("Bullet options")]
+    public GameObject bulletSprite; // what sprite to use as a bullet
+    public float bulletSpeed;
+    private float lastFire;
+    public float fireDelay;
+
+    [Header("hallo dit is mijn header")]
+    public float randomnummer;
+
+    private void FixedUpdate()
+    {
+        Death();
+        // horizontal player movement
+        Vector3 movementX = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+        transform.position += movementX * Time.deltaTime * movementSpeed;
+        // vertical player movement
+        Vector3 movementY = new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+        transform.position += movementY * Time.deltaTime * movementSpeed;
+        // horizontal bullet movement
+        float bulletMovementX = Input.GetAxisRaw("ShootHorizontal");
+        // vertical bullet movement
+        float bulletMovementY = Input.GetAxisRaw("ShootVertical");
+
+        if ((bulletMovementX != 0 || bulletMovementY != 0 ) && Time.time > lastFire + fireDelay)
+        {
+            // shoot 
+            shoot(bulletMovementX, bulletMovementY);
+            lastFire = Time.time;
+        }
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("projectile"))
+        {
+            health -= projectileDamage;
+        }
+    }
+
+    public void Death()
+    {
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void shoot(float x, float y)
+    {
+        GameObject bullet = Instantiate(bulletSprite, transform.position, transform.rotation);
+        bullet.AddComponent<Rigidbody2D>().gravityScale = 0;
+        bullet.GetComponent<Rigidbody2D>().velocity = new Vector3(
+            (x < 0) ? Mathf.Floor(x) * bulletSpeed : Mathf.Ceil(x) * bulletSpeed,
+            (y < 0) ? Mathf.Floor(y) * bulletSpeed : Mathf.Ceil(y) * bulletSpeed, 0);
+            
+    }
+}
